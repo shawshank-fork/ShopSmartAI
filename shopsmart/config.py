485458639c2824f58c426f43967cc7e4d387b82a
shawshank <shashanklakhaiyar71@gmail.com>
2026-01-01 16:00:13 +0530

    data conversion

--- shopsmart/config.py
+++ shopsmart/config.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-class config:
+class Config:
     ASTRA_DB_API_ENDPOINT = os.getenv("ASTRA_DB_API_ENDPOINT")
     ASTRA_DB_APPLICATION_TOKEN = os.getenv("ASTRA_DB_APPLICATION_TOKEN")
     ASTRA_DB_KEYSPACE = os.getenv("ASTRA_DB_KEYSPACE")
