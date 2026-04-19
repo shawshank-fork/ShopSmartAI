b75bf78dfafcce632a44901eac0fb044af6f1323
shawshank <shashanklakhaiyar71@gmail.com>
2026-04-19 18:10:04 +0530

    feat: update data ingestion to use API with CSV fallback

---
+++ shopsmart/csv_fallback.py
@@ -0,0 +1,18 @@
+import pandas as pd
+from langchain_core.documents import Document
+
+class CSVDataConverter:
+    def __init__(self,file_path: str):
+        self.file_path = file_path
+
+    def convert(self):
+        df = pd.read_csv(self.file_path)[["product_title", "price", "review"]]
+
+        docs = [
+            Document(
+                page_content=f"Product: {row['product_title']}\nPrice: Rs {row['price']}\nReview: {row['review']}",
+                metadata={"product_name": row["product_title"], "price": row["price"]}
+            )
+            for _, row in df.iterrows()
+        ]
+        return docs    
