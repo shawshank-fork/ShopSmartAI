4fb445b56ba8405c5894c66dbe267d882b5c5089
shawshank <shashanklakhaiyar71@gmail.com>
2026-04-19 17:52:52 +0530

    updated data ingestion for handling live api and added fallback to csv data when api is down

--- shopsmart/data_ingestion.py
+++ shopsmart/data_ingestion.py
@@ -1,7 +1,10 @@
 from langchain_astradb import AstraDBVectorStore
 from langchain_huggingface import HuggingFaceEndpointEmbeddings
-from shopsmart.data_converter import DataConverter
+from shopsmart.data_converter import APIDataConverter
 from shopsmart.config import Config
+from utils.logger import get_logger
+
+logger = get_logger(__name__)
 
 class DataIngestor:
     def __init__(self):
@@ -18,11 +21,18 @@ class DataIngestor:
 
         ## Main method for this class is to convert the docs into embeddings
     def ingest(self,load_existing=True): #load existing true means if vector store already has data then just load it
-            if load_existing==True:          # we will set load_existing to false if we have replaced the csv file to a new file, so that if it will perform all the steps again
-                return self.vstore
+            if load_existing:
+                logger.info("Loading existing vector store from AstraDB")          # we will set load_existing to false if we have replaced the csv file to a new file, so that if it will perform all the steps again
+                return self.vstore    
             
-            docs = DataConverter("data/flipkart_product_review.csv").convert() #Tales csv fo;s and converts them into docs
+            try:
+                logger.info("Fetching products from DummyJSON API....")
+            except Exception as e:
+                logger.warning(f"API fetch failed ({e}), falling back to CSV..")
+                from shopsmart.csv_fallback import CSVDataConverter
+                docs = CSVDataConverter("data/flipkart_product_review.csv").convert()
+                logger.info(f"Loaded {len(docs)} products from CSV fallback")
 
-            self.vstore.add_documents(docs) #adding docs to vector store
-            return self.vstore #Returning the vector store after adding documents
-## langchain cant directly acces csv so we need to cnvert data into document
+            self.vstore.add_documents(docs)
+            logger.info("Data ingestion complete!")
+            return self.vstore    
